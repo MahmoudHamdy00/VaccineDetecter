353410ffe78cfa09b6b01585faaceead61d8e812
MahmoudHamdy00 <mahmoudh.morsy@gmail.com>
2022-03-12 15:19:18 +0200

    implement email sender service and inject it using dependency injection

--- VaccineDetecter-Backend/Program.cs
+++ VaccineDetecter-Backend/Program.cs
@@ -1,3 +1,5 @@
+using VaccineDetecter_Backend.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+//
+var myEmail = builder.Configuration["email"];
+var myPassword = builder.Configuration["emailpassword"];
+var SMTPServerAddress = builder.Configuration["SMTPServerAddress"];
+var mailSubmissionPort = Convert.ToInt32(builder.Configuration["mailSubmissionPort"]);
+builder.Services.AddTransient<IEmailSenderService, EmailSenderService>(op => new EmailSenderService(myEmail, myPassword, SMTPServerAddress, mailSubmissionPort));
+//
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ VaccineDetecter-Backend/Services/EmailSenderService.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Mail;
+
+namespace VaccineDetecter_Backend.Services
+{
+    public class EmailSenderService : IEmailSenderService
+    {
+        private readonly string _email;
+        private readonly string _password;
+        private readonly string _SMTPServerAddress;
+        private readonly int _mailSubmissionPort;
+
+        public EmailSenderService(string email, string password, string SMTPServerAddress, int mailSubmissionPort) {
+            _email = email;
+            _password = password;
+            _SMTPServerAddress = SMTPServerAddress;
+            _mailSubmissionPort = mailSubmissionPort;
+        }
+        public void SendEmail(string emailTo, string MailSubject, string MailBody, bool isHTML = true) {
+            MailMessage mailMessage = new MailMessage(_email, emailTo);
+            mailMessage.Subject = MailSubject;
+            mailMessage.Body = MailBody;
+            mailMessage.IsBodyHtml = isHTML;
+            SmtpClient smtpClient = new SmtpClient(_SMTPServerAddress, _mailSubmissionPort);
+            smtpClient.Credentials = new System.Net.NetworkCredential() {
+                UserName = _email,
+                Password = _password
+            };
+            smtpClient.EnableSsl = true;
+            smtpClient.Send(mailMessage);
+            return;
+        }
+    }
+}
