001ed4f8b022eff1b3adfc9941d91a3cced03740
NaglaEssam12 <59181709+NaglaEssam12@users.noreply.github.com>
2022-03-13 16:57:22 +0200

    Update App.js

--- VaccineDetecter-Frontend/src/App.js
+++ VaccineDetecter-Frontend/src/App.js
@@ -17,46 +17,44 @@ function App() {
       backgroundAttachment: 'fixed',
       
     }}>
-<h1 style={{marginLeft:'20px',color:'purple' ,fontStyle:'italic'}}>
+<h1 class= "headerStyle1" style={{fontStyle:'italic'}}>
   Vaccine Recommendation
   </h1>
-  <h2 style={{marginLeft:'20px',color:'crimson' ,fontStyle:'oblique'}}> 
+  <h2  class= "headerStyle2"> 
   Do not know what is the vaccine type that is suitable for you ? 
   </h2>
-  <h1 style={{marginLeft:'20px',color:'crimson' ,fontStyle:'oblique'}}>
+  <h1 class="headerStyle2">
     we have got your back ..
   </h1>
-  <p style={{marginLeft:'20px',color:'darkblue' ,fontStyle:'oblique'}}>
+  <p class="headerStyle2" style={{color:'darkblue' ,fontStyle:'oblique'}}>
     With our claboration with alfa scan,
     we will recommend you the most suitable vaccine type depending on 
     the required blood tests.
   </p>
 
       <img class="sticky" src={alfascan} alt="Avatar" height="150px" />
-      <div style={{ justifyContent:'center',
-      display:'flex'
+      <div style={{ justifyContent:'center',display:'flex'}} >
 
-}} >
       <form class="inputs">
       <label for="inputEmail" class="form-label">Email : </label>
       <input type="email" id="inputEmail" class="form-control"  />
 
       <label for="inputAge" class="form-label">Age :</label>
       <input type="text" id="inputAge" class="form-control"  />
+
       <div class="input-group mb-3" style={{marginTop:'50px'}}>
-  <label class="input-group-text" for="inputGroupSelect01">Gender</label>
-  <select class="form-select" id="inputGroupSelect01">
-    <option selected>Choose...</option>
-    <option value="Male">Male</option>
-    <option value="Female">Female</option>
-  </select>
+      <label class="input-group-text" for="inputGroupSelect01">Gender</label>
+      <select class="form-select" id="inputGroupSelect01">
+        <option selected>Choose...</option>
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
+      </select>
 </div>
       <label for="inputwbc" class="form-label">White blood cells :</label>
       <input type="text" id="inputwbc" class="form-control"  />
      
 
-      <br>
-      </br>
+      <br></br>
       <label for="inputrbc" class="form-label">Red blood cells :</label>
       <input type="text" id="inputrbc" class="form-control"  />
 
@@ -74,8 +72,9 @@ function App() {
 </div>
 <footer class="site-footer"><div class="container">
   <a id="scroll-up" href="#"><i class="fa fa-angle-up"></i></a>
-  <div class="row"><div class="col-md-4 col-sm-4">
-    <p>Sponsord by Alfa Scan</p>
+  <div class="row">
+    <div class="col-md-4 col-sm-4">
+      <p>Sponsord by Alfa Scan</p>
     </div>
     <div class="col-md-8 col-sm-8">
       <br></br>
