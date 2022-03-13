572bcf6d2920f86c23e7c7e79269685d8518de4d
MahmoudHamdy00 <mahmoudh.morsy@gmail.com>
2022-03-13 18:34:45 +0200

    Add validation to back end

---
+++ VaccineDetecter-Backend/Utility/Validator.cs
@@ -0,0 +1,18 @@
+﻿namespace VaccineDetecter_Backend.Utility
+{
+    public class Validator
+    {
+        public static bool IsValidEmail(string email) {
+            try {
+                if (email.EndsWith(".")) {
+                    return false;
+                }
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch {
+                return false;
+            }
+        }
+    }
+}
